b4718f30cc66eb6ddcb5688acd58169ae26a9eb1
IFKabir <ifk.ishanfalak@gmail.com>
2026-03-04 21:25:45 +0530

    AbCdEfGhIjKlMnOpQrStUvWxYz

--- boot.asm
+++ boot.asm
@@ -1,17 +1,20 @@
 mov ah, 0x0e
-mov al, 65
+mov al, 'A'
 
-loop:
+caps:
     int 0x10
-    inc al
-    cmp al, 91
+    jmp small
+
+small:
+    add al, 33
+    int 0x10
+    cmp al, 122
     je end
-    jmp loop
+    sub al, 31
+    jmp caps
 
 end:
     jmp $
-
-jmp $
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
